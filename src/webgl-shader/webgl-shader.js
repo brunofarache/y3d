ca680e9717976382b0d9e4a7af02552f719acdff
brunofarache <bruno.farache@liferay.com>
2013-01-01 22:37:47 -0300

    source formatting

--- src/webgl-shader/webgl-shader.js
+++ src/webgl-shader/webgl-shader.js
@@ -1,127 +1,129 @@
 YUI.add('webgl-shader', function(Y) {
 	var colorProgram = null,
-		textureProgram = null;
+		textureProgram = null,
 
-	var fragmentShaderSource = [
-		'precision mediump float;',
+		fragmentShaderSource = [
+			'precision mediump float;',
 
-		'varying vec4 fragmentColor;',
-
-		'#ifdef USE_TEXTURE',
-			'varying vec2 vertexTextureCoordinates;',
-			'uniform sampler2D sampler;',
-		'#endif',
-
-		'void main(void) {',
-			'gl_FragColor = fragmentColor;',
+			'varying vec4 fragmentColor;',
 
 			'#ifdef USE_TEXTURE',
-				'gl_FragColor = gl_FragColor * texture2D(sampler, vertexTextureCoordinates);',
+				'varying vec2 vertexTextureCoordinates;',
+				'uniform sampler2D sampler;',
 			'#endif',
-		'}'
-	].join('\n');
 
-	var vertexShaderSource = [
-		'attribute vec3 vertexPosition;',
-		'attribute vec4 vertexColor;',
+			'void main(void) {',
+				'gl_FragColor = fragmentColor;',
 
-		'#ifdef USE_TEXTURE',
-			'attribute vec2 textureCoordinates;',
-			'varying vec2 vertexTextureCoordinates;',
-		'#endif',
+				'#ifdef USE_TEXTURE',
+					'gl_FragColor = gl_FragColor * texture2D(sampler, vertexTextureCoordinates);',
+				'#endif',
+			'}'
+		].join('\n'),
 
-		'uniform mat4 projectionMatrix;',
-		'uniform mat4 modelViewMatrix;',
+		vertexShaderSource = [
+			'attribute vec3 vertexPosition;',
+			'attribute vec4 vertexColor;',
 
-		'varying vec4 fragmentColor;',
+			'#ifdef USE_TEXTURE',
+				'attribute vec2 textureCoordinates;',
+				'varying vec2 vertexTextureCoordinates;',
+			'#endif',
+
+			'uniform mat4 projectionMatrix;',
+			'uniform mat4 modelViewMatrix;',
+
+			'varying vec4 fragmentColor;',
 		
-		'void main(void) {',
-			'gl_Position = projectionMatrix * modelViewMatrix * vec4(vertexPosition, 1.0);',
-			'fragmentColor = vertexColor;',
+			'void main(void) {',
+				'gl_Position = projectionMatrix * modelViewMatrix * vec4(vertexPosition, 1.0);',
+				'fragmentColor = vertexColor;',
 
-			'#ifdef USE_TEXTURE',
-				'vertexTextureCoordinates = textureCoordinates;',
-			'#endif',
-		'}'
-	].join('\n');
+				'#ifdef USE_TEXTURE',
+					'vertexTextureCoordinates = textureCoordinates;',
+				'#endif',
+			'}'
+		].join('\n');
 	
 	var Shader = Y.namespace('Shader');
 
-	Shader.compile = function(context, type, constants) {
-		var shader, source;
+	Y.Shader = {
+		compile: function(context, type, constants) {
+			var shader, source;
 
-		if (type === 'fragment') {
-			shader = context.createShader(context.FRAGMENT_SHADER);
-			source = fragmentShaderSource;
-		}
-		else if (type === 'vertex') {
-			shader = context.createShader(context.VERTEX_SHADER);
-			source = vertexShaderSource;
-		}
+			if (type === 'fragment') {
+				shader = context.createShader(context.FRAGMENT_SHADER);
+				source = fragmentShaderSource;
+			}
+			else if (type === 'vertex') {
+				shader = context.createShader(context.VERTEX_SHADER);
+				source = vertexShaderSource;
+			}
 
-		source = [constants, source].join('\n');
+			source = [constants, source].join('\n');
 
-		context.shaderSource(shader, source);
-		context.compileShader(shader);
+			context.shaderSource(shader, source);
+			context.compileShader(shader);
 
-		if (!context.getShaderParameter(shader, context.COMPILE_STATUS)) {
-			console.log(context.getShaderInfoLog(shader));
+			if (!context.getShaderParameter(shader, context.COMPILE_STATUS)) {
+				console.log(context.getShaderInfoLog(shader));
 
-			return null;
-		}
+				return null;
+			}
 
-		return shader;		
-	};
+			return shader;		
+		},
 
-	Shader.getColorProgram = function(context) {
-		if (colorProgram != null) {
+		getColorProgram: function(context) {
+			if (colorProgram != null) {
+				return colorProgram;
+			}
+
+			colorProgram = Y.Shader.link(context, []);
+
 			return colorProgram;
-		}
+		},
 
-		colorProgram = Shader.link(context, []);
+		getTextureProgram: function(context) {
+			if (textureProgram != null) {
+				return textureProgram;
+			}
 
-		return colorProgram;
-	};
+			textureProgram = Y.Shader.link(context, ['#define USE_TEXTURE']);
 
-	Shader.getTextureProgram = function(context) {
-		if (textureProgram != null) {
+			textureProgram.textureCoordinatesAttribute = context.getAttribLocation(textureProgram, "textureCoordinates");
+			context.enableVertexAttribArray(textureProgram.textureCoordinatesAttribute);
+
+			textureProgram.samplerUniform = context.getUniformLocation(textureProgram, "sampler");
+
 			return textureProgram;
+		},
+
+		link: function(context, constants) {
+			var fragmentShader = Y.Shader.compile(context, 'fragment', constants),
+				vertexShader = Y.Shader.compile(context, 'vertex', constants);
+
+			var program = context.createProgram();
+
+			context.attachShader(program, fragmentShader);
+			context.attachShader(program, vertexShader);
+
+			context.linkProgram(program);
+
+			if (!context.getProgramParameter(program, context.LINK_STATUS)) {
+				console.log("Could not link shaders");
+			}
+
+			program.vertexPositionAttribute = context.getAttribLocation(program, "vertexPosition");
+			context.enableVertexAttribArray(program.vertexPositionAttribute);
+
+			program.vertexColorAttribute = context.getAttribLocation(program, "vertexColor");
+			context.enableVertexAttribArray(program.vertexColorAttribute);
+
+			program.projectionMatrixUniform = context.getUniformLocation(program, "projectionMatrix");
+			program.modelViewMatrixUniform = context.getUniformLocation(program, "modelViewMatrix");
+
+			return program;
 		}
-
-		textureProgram = Shader.link(context, ['#define USE_TEXTURE']);
-
-		textureProgram.textureCoordinatesAttribute = context.getAttribLocation(textureProgram, "textureCoordinates");
-        context.enableVertexAttribArray(textureProgram.textureCoordinatesAttribute);
-
-        textureProgram.samplerUniform = context.getUniformLocation(textureProgram, "sampler");
-
-		return textureProgram;
-	};
-
-	Shader.link = function(context, constants) {
-		var fragmentShader = Shader.compile(context, 'fragment', constants),
-			vertexShader = Shader.compile(context, 'vertex', constants);
-
-		var program = context.createProgram();
-
-		context.attachShader(program, fragmentShader);
-		context.attachShader(program, vertexShader);
-
-		context.linkProgram(program);
-
-		if (!context.getProgramParameter(program, context.LINK_STATUS)) {
-            console.log("Could not link shaders");
-        }
-
-        program.vertexPositionAttribute = context.getAttribLocation(program, "vertexPosition");
-        context.enableVertexAttribArray(program.vertexPositionAttribute);
-
-        program.vertexColorAttribute = context.getAttribLocation(program, "vertexColor");
-    	context.enableVertexAttribArray(program.vertexColorAttribute);
-
-        program.projectionMatrixUniform = context.getUniformLocation(program, "projectionMatrix");
-        program.modelViewMatrixUniform = context.getUniformLocation(program, "modelViewMatrix");
-
-        return program;
 	};
 }, '1.0', {requires: ['node-base']});
