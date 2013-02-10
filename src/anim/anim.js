f70c75f3a455e63d57710c23ba29b100890df6f7
brunofarache <bruno.farache@liferay.com>
2013-02-10 15:59:29 -0300

    animation module

---
+++ src/anim/anim.js
@@ -0,0 +1,37 @@
+YUI.add('webgl-anim', function(Y) {
+	var requestAnimFrame =
+		window.requestAnimationFrame 	   || 
+		window.webkitRequestAnimationFrame || 
+		window.mozRequestAnimationFrame    || 
+		window.oRequestAnimationFrame      || 
+		window.msRequestAnimationFrame     || 
+		function(callback) {
+			window.setTimeout(callback, 1000 / 60);
+		};
+
+	function WebGLAnim(scene, animationFn, debugFn) {
+		this.scene = scene;
+		this.animationFn = animationFn;
+		this.debugFn = debugFn;
+	}
+
+	WebGLAnim.prototype = {
+		start: function() {
+			this.animationFn();
+			this.scene.render();
+			
+			if (this.debugFn) {
+				this.last = this.now;
+				this.now = (new Date()).getTime();
+
+				var elapsed = this.now - this.last;
+
+				this.debugFn(parseInt(1000 / elapsed));
+			}
+
+			requestAnimFrame(Y.bind(this.start, this));
+		}
+	};
+
+	Y.WebGLAnim = WebGLAnim;
+}, '1.0', {});
