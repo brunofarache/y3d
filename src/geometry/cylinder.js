09a8a86deaee443e3ff7a955adf3eb178516646d
brunofarache <bruno.farache@liferay.com>
2013-05-13 22:27:29 -0300

    cylinder, missing top and botton circles

--- src/geometry/cylinder.js
+++ src/geometry/cylinder.js
@@ -1,9 +1,10 @@
-YUI.add('webgl-sphere', function(Y) {
+YUI.add('webgl-cylinder', function(Y) {
 	var Lang = Y.Lang;
 
-	Y.Sphere = Y.Base.create('sphere', Y.Geometry, [], {
+	Y.Cylinder = Y.Base.create('cylinder', Y.Geometry, [], {
 		initializer: function() {
 			var instance = this,
+				height = instance.get('height'),
 				heightBands = instance.get('heightBands'),
 				indices = instance.get('indices'),
 				normals = instance.get('normals'),
@@ -12,26 +13,29 @@ YUI.add('webgl-sphere', function(Y) {
 				vertices = instance.get('vertices'),
 				widthBands = instance.get('widthBands');
 			
+			var circle = [];
+
+			for (var i = 0; i <= widthBands; i++) {
+				var theta = i * 2 * Math.PI / widthBands,
+					x = Math.cos(theta),
+					z = Math.sin(theta);
+
+				circle.push(x, z);
+			}
+
 			for (var i = 0; i <= heightBands; i++) {
-				var theta = i * Math.PI / heightBands,
-					sinTheta = Math.sin(theta),
-					cosTheta = Math.cos(theta);
+				var y = (height / 2) + (height / heightBands) * i;
 
-				for (var j = 0; j <= widthBands; j++) {
-					var phi = j * 2 * Math.PI / widthBands,
-						sinPhi = Math.sin(phi),
-						cosPhi = Math.cos(phi);
-
-					var x = cosPhi * sinTheta,
-						y = cosTheta,
-						z = sinPhi * sinTheta;					
+				for (var j = 0; j < circle.length; j = j + 2) {
+					var x = circle[j],
+						z = circle[j + 1];
 
 					vertices.push(radius * x);
-					vertices.push(radius * y);
+					vertices.push(y);
 					vertices.push(radius * z);
 
 					normals.push(x);
-					normals.push(y);
+					normals.push(0);
 					normals.push(z);
 
 					var u = 1 - (j / widthBands),
@@ -59,6 +63,11 @@ YUI.add('webgl-sphere', function(Y) {
 		}
 	}, {
 		ATTRS: {
+			height: {
+				value: 4,
+				validator: Lang.isNumber
+			},
+
 			heightBands: {
 				value: 32,
 				validator: Lang.isNumber
