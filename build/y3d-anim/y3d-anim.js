8ba3224199f24aa023794d7c09282325194fc046
Bruno Farache <bruno.farache@liferay.com>
2013-06-24 19:38:16 -0300

    build

--- build/y3d-anim/y3d-anim.js
+++ build/y3d-anim/y3d-anim.js
@@ -1,19 +1,41 @@
 YUI.add('y3d-anim', function (Y, NAME) {
 
-var requestAnimFrame =
-	window.requestAnimationFrame		||
-	window.webkitRequestAnimationFrame	||
-	window.mozRequestAnimationFrame		||
-	window.oRequestAnimationFrame		||
-	window.msRequestAnimationFrame		||
-	function(callback) {
-		window.setTimeout(callback, 1000 / 60);
+var lastTime = 0,
+	vendors = ['webkit', 'moz'],
+	x;
+
+for (x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+	window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+
+	window.cancelAnimationFrame =
+		window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+}
+
+if (!window.requestAnimationFrame) {
+	window.requestAnimationFrame = function(callback) {
+		var currTime = new Date().getTime(),
+			timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+			id = window.setTimeout(function() {
+				callback(currTime + timeToCall);
+			}, timeToCall);
+
+		lastTime = currTime + timeToCall;
+
+		return id;
 	};
+}
+
+if (!window.cancelAnimationFrame) {
+	window.cancelAnimationFrame = function(id) {
+		clearTimeout(id);
+	};
+}
 
 function WebGLAnim(scene, animationFn, debugFn) {
 	this.scene = scene;
 	this.animationFn = animationFn;
 	this.debugFn = debugFn;
+	this.requestId = 0;
 }
 
 WebGLAnim.prototype = {
@@ -30,7 +52,11 @@ WebGLAnim.prototype = {
 			this.debugFn(parseInt(1000 / elapsed, 10));
 		}
 
-		requestAnimFrame(Y.bind(this.start, this));
+		this.requestId = requestAnimationFrame(Y.bind(this.start, this));
+	},
+
+	stop: function() {
+		cancelAnimationFrame(this.requestId);
 	}
 };
 
