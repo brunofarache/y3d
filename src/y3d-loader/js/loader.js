3689d63bf9d50172be373b4fc5c709fed54afd3f
Bruno Farache <bruno.farache@liferay.com>
2013-08-10 18:52:57 -0300

    #3 loading from file

--- src/y3d-loader/js/loader.js
+++ src/y3d-loader/js/loader.js
@@ -9,29 +9,30 @@ Y.Loader = Y.Base.create('loader', Y.Base, [], {
 			normals = geometry.get('normals'),
 			src = instance.get('src'),
 			lines = src.split('\n'),
-			i, line, values;
+			i, line, values,
+			v1, v2, v3, i1, i2, i3, i4;
 
 		for (i = 0; i < lines.length; i++) {
 			line = lines[i].trim();
 			values = line.split(/\s+/);
 
 			if (line.indexOf('v ') === 0) {
-				var v1 = parseFloat(values[1], 10),
-					v2 = parseFloat(values[2], 10),
-					v3 = parseFloat(values[3], 10);
+				v1 = parseFloat(values[1], 10);
+				v2 = parseFloat(values[2], 10);
+				v3 = parseFloat(values[3], 10);
 
 				vertices.push(v1, v2, v3);
 			}
 			else if (line.indexOf('f ') === 0) {
-				var i1 = parseInt(values[1], 10) - 1,
-					i2 = parseInt(values[2], 10) - 1,
-					i3 = parseInt(values[3], 10) - 1;
+				i1 = parseInt(values[1], 10) - 1;
+				i2 = parseInt(values[2], 10) - 1;
+				i3 = parseInt(values[3], 10) - 1;
 
 				if (values.length === 4) {
 					indices.push(i1, i2, i3);
 				}
 				else if (values.length === 5) {
-					var i4 = parseInt(values[4], 10) - 1;
+					i4 = parseInt(values[4], 10) - 1;
 
 					indices.push(i1, i2, i3, i1, i3, i4);
 				}
@@ -53,28 +54,7 @@ Y.Loader = Y.Base.create('loader', Y.Base, [], {
 		},
 
 		src: {
-			value: [
-				'# Vertex list',
-				'',
-				'v -0.5 -0.5 0.5',
-				'v -0.5 -0.5 -0.5',
-				'v -0.5 0.5 -0.5',
-				'v -0.5 0.5 0.5',
-				'v 0.5 -0.5 0.5',
-				'v 0.5 -0.5 -0.5',
-				'v 0.5 0.5 -0.5',
-				'v 0.5 0.5 0.5',
-				'',
-				'# Point/Line/Face list',
-				'',
-				'f 4 3 2 1',
-				'f 2 6 5 1',
-				'f 3 7 6 2',
-				'f 8 7 3 4',
-				'f 5 8 4 1',
-				'f 6 7 8 5'
-			].join('\n'),
-
+			value: '',
 			validator: Lang.isString
 		}
 	}
