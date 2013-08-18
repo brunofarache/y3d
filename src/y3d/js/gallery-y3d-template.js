1f68493801a555a43568025dbf0262249f3bb8da
Bruno Farache <bruno.farache@liferay.com>
2013-08-18 10:59:27 -0300

    automate gallery-y3d build

---
+++ src/y3d/js/gallery-y3d-template.js
@@ -0,0 +1,13 @@
+{
+	"builds": {
+	},
+
+	"rollups": {
+		"gallery-y3d": {
+			"config": {
+				"use": @GALLERY_MODULES@
+			},
+			"files": @GALLERY_MODULES@
+		}
+	}
+}
--- src/y3d/scripts/build.js
+++ src/y3d/scripts/build.js
@@ -2,10 +2,27 @@
 
 var fs = require('fs'),
 	modules = require('../js/y3d-config-modules.json'),
-	content;
+	content,
+	gallery = [];
 
 content = fs.readFileSync('./js/y3d-config-template.js').toString();
-
 content = content.replace('@MODULES@', JSON.stringify(modules, null, '\t'));
 
 fs.writeFileSync('./js/y3d.js', content);
+
+for (module in modules) {
+	if (module === 'gallery-y3d') {
+		continue;
+	}
+
+	if (modules[module]['use'] !== undefined) {
+		continue;
+	}
+
+	gallery.push(module);
+}
+
+content = fs.readFileSync('./js/gallery-y3d-template.js').toString();
+content = content.replace(new RegExp('@GALLERY_MODULES@', 'g'), JSON.stringify(gallery, null, '\t\t\t\t\t'));
+
+fs.writeFileSync('../gallery-y3d/build.json', content);
