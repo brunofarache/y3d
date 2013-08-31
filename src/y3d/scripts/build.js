754d80c55c4c2f8420214c11c56ed8f92628270d
Bruno Farache <bruno.farache@liferay.com>
2013-08-31 01:41:50 -0300

    remove gallery-y3d build script from y3d

--- src/y3d/scripts/build.js
+++ src/y3d/scripts/build.js
@@ -9,20 +9,3 @@ content = fs.readFileSync('./js/y3d-config-template.js').toString();
 content = content.replace('@MODULES@', JSON.stringify(modules, null, '\t'));
 
 fs.writeFileSync('./js/y3d.js', content);
-
-for (module in modules) {
-	if (module === 'gallery-y3d') {
-		continue;
-	}
-
-	if (modules[module]['use'] !== undefined) {
-		continue;
-	}
-
-	gallery.push(module);
-}
-
-content = fs.readFileSync('./js/gallery-y3d-template.js').toString();
-content = content.replace(new RegExp('@GALLERY_MODULES@', 'g'), JSON.stringify(gallery, null, '\t\t\t\t\t'));
-
-fs.writeFileSync('../gallery-y3d/build.json', content);
